7cb6b5fba701cfebd3b46ae11303942a5a0778c7
Ilya <kulak1224@gmail.com>
2024-07-09 18:43:13 +0300

    CLI

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,20 @@
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-const generateContacts = async (number) => {};
+import fs from 'fs/promises';
+
+const generateContacts = async (number) => {
+  const newItemsToPush = [];
+  for (let i = 0; i < number; i++) {
+    newItemsToPush.push(createFakeContact());
+  }
+
+  const contacts = JSON.parse(await fs.readFile(PATH_DB));
+
+  fs.writeFile(
+    PATH_DB,
+    JSON.stringify([...contacts, ...newItemsToPush], null, 2),
+  );
+};
 
 generateContacts(5);
